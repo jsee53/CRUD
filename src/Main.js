bff026c657220c746170663599c1ad8feb7c7e87
SeeYoung Jin <jsee53@naver.com>
2023-01-11 13:46:38 +0900

    third commit

--- src/Main.js
+++ src/Main.js
@@ -7,17 +7,33 @@ const template = require("./Template.js");
 var app = http.createServer(function (request, response) {
   var request_url = request.url;
   var pathname = url.parse(request_url, true).pathname;
+  var queryData = url.parse(request_url, true).query;
+  var title; //해당 page의 제목
+  var list; //파일 목록
+  var description; //파일의 내용
+  var return_HTML; //출력 화면
 
-  //메인 화면
+  //접속이 성공할 때
   if (pathname === "/") {
-    console.log(
-      "request_url: " + `${request_url}` + " pathname: " + `${pathname}`
-    );
-    response.writeHead(200);
-    response.end(template);
-  } else {
-    response.writeHead(200);
-    response.end("/Main.html");
+    //메인 화면
+    if (queryData.id === undefined) {
+      fs.readdir("../file", function (error, filelist) {
+        title = "게시판";
+        description = "자유롭게 작성하는 공간";
+        list = template.LIST(filelist);
+        return_HTML = template.HTML(title, list, description);
+        response.writeHead(200);
+        response.end(return_HTML);
+      });
+    } else {
+      response.writeHead(200);
+      response.end("??");
+    }
+  }
+  //접속이 실패할 때
+  else {
+    response.writeHead(404);
+    response.end("Not found!");
   }
 });
 
--- src/Template.js
+++ src/Template.js
@@ -1,18 +1,53 @@
 const { mainModule } = require("process");
 
-var html = function () {
-  return `  
-<html lang="ko">
-<head>
-    <meta charset="UTF-8" />
-    <title>tiele</title>
-</head>
-<body>
-    게시판
-    <div>list</div>
-</body>
-</html>
-`;
+var template = {
+  HTML: function (title, list, description) {
+    return `  
+    <html lang="ko">
+    <head>
+      <meta charset="UTF-8" />
+      <link rel="stylesheet" href="Main.css" />
+      <title>${title}</title>
+    </head>
+    <body>
+      <div
+        class="title"
+        style="
+          text-align: center;
+          margin-top: 50px;
+          font-size: 40px;
+          font-weight: bold;
+          color: aquamarine;
+        "
+      >
+        게시판
+      </div>
+      <div
+        class="contentBox"
+        style="text-align: center; margin-top: 100px; display: inline-block"
+      >
+        <div class="content"></div>
+        <div>
+          <button
+            class="createBtn"
+            style="background-color: white; border: 1px solid #ddd"
+          >
+            글 쓰기
+          </button>
+        </div>
+        <div class="list">${list}</div>
+      </div>
+    </body>
+  </html>
+  `;
+  },
+  LIST: function (filelist) {
+    var list = "<ul>";
+    for (var i = 0; i < filelist.length; i++) {
+      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    }
+    list += "</ul>";
+    return list;
+  },
 };
-
-module.exports = html();
+module.exports = template;
