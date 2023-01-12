0b92591b73892b53018f3f72bfe1d2d44267c242
SeeYoung Jin <jsee53@naver.com>
2023-01-12 15:08:37 +0900

    error check

--- src/Main.js
+++ src/Main.js
@@ -8,9 +8,9 @@ var app = http.createServer(function (request, response) {
   var request_url = request.url;
   var pathname = url.parse(request_url, true).pathname;
   var queryData = url.parse(request_url, true).query;
-  var title; //해당 page의 제목
-  var list; //파일 목록
-  var description; //파일의 내용
+  var title; //파일의 제목
+  var content; //파일의 내용
+  var date; //파일 생성 날짜
   var return_HTML; //출력 화면
 
   //접속이 성공할 때
@@ -18,10 +18,9 @@ var app = http.createServer(function (request, response) {
     //메인 화면
     if (queryData.id === undefined) {
       fs.readdir("../file", function (error, filelist) {
-        title = "게시판";
-        description = "자유롭게 작성하는 공간";
-        list = template.LIST(filelist);
-        return_HTML = template.HTML(title, list, description);
+        date = "2023-01-12";
+        content = template.CONTENT(filelist, date);
+        return_HTML = template.HTML(title, content);
         response.writeHead(200);
         response.end(return_HTML);
       });
--- src/Template.js
+++ src/Template.js
@@ -1,53 +1,159 @@
 const { mainModule } = require("process");
 
 var template = {
-  HTML: function (title, list, description) {
+  HTML: function (title, content) {
     return `  
     <html lang="ko">
-    <head>
-      <meta charset="UTF-8" />
-      <link rel="stylesheet" href="Main.css" />
-      <title>${title}</title>
-    </head>
-    <body>
-      <div
-        class="title"
-        style="
-          text-align: center;
-          margin-top: 50px;
-          font-size: 40px;
-          font-weight: bold;
-          color: aquamarine;
-        "
-      >
-        게시판
-      </div>
-      <div
-        class="contentBox"
-        style="text-align: center; margin-top: 100px; display: inline-block"
-      >
-        <div class="content"></div>
-        <div>
-          <button
-            class="createBtn"
-            style="background-color: white; border: 1px solid #ddd"
-          >
-            글 쓰기
-          </button>
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="Main.css" />
+    <title>${title}</title>
+  </head>
+  <body>
+    <nav class="title">자유로운 공간</nav>
+    <div class="window">
+      <div class="board">
+        <div class="boardBar">
+          <div class="board_title">Boards</div>
+          <div class="create">
+            <button class="createBtn"><a href="/create">+ Create a new Board</a></button>
+          </div>
         </div>
-        <div class="list">${list}</div>
+        <div class="content">
+          <div>
+            <table class="content_table">
+              <tbody>
+               <tr>
+               <td>ID</td>
+               <td>Title</td>
+               <td>Contents</td>
+                <td>Create Date</td>
+                <td></td>
+              </tr>
+              ${content}
+            </table>
+          </div>
+        </div>
       </div>
-    </body>
-  </html>
+    </div>
+  </body>
+</html>
+
+<style>
+  html {
+    margin: auto;
+  }
+  body {
+    margin: 0;
+    background-color: rgb(242, 240, 240);
+  }
+
+  .title {
+    background-color: black;
+    padding: 20px;
+    padding-left: 100px;
+    font-size: 25px;
+    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
+    font-weight: bold;
+    color: white;
+  }
+
+  .window {
+    padding: 30px;
+  }
+
+  .board {
+    height: 100%;
+    background-color: aliceblue;
+  }
+
+  .board_title {
+    font-size: 30px;
+    padding-top: 20px;
+    margin-left: 20px;
+  }
+
+  .create {
+    text-align: center;
+    float: right;
+    margin-right: 30px;
+    margin-top: 30px;
+  }
+
+  .createBtn {
+    color: white;
+    font-size: 15px;
+    background-color: cornflowerblue;
+    border: none;
+    padding: 10px;
+  }
+
+  .content {
+    margin-top: 100px;
+  }
+
+  .content_table {
+    border-collapse: collapse;
+    margin-left: 40px;
+  }
+
+  tr {
+    border-top: 1px solid #ddd;
+  }
+
+  td {
+    padding: 20px;
+    padding-left: 120px;
+  }
+
+  .viewBtn {
+    border: none;
+    background-color: rgb(61, 184, 191);
+    color: white;
+    padding: 8px;
+  }
+
+  .editBtn {
+    border: none;
+    background-color: cornflowerblue;
+    color: white;
+    padding: 8px;
+  }
+
+  .delectBtn {
+    border: none;
+    background-color: red;
+    color: white;
+    padding: 8px;
+  }
+
+  a{
+    color:black;
+    text-decoration: none;
+  }
+
+  button a {
+    color: white;
+  }
+</style>
+
   `;
   },
-  LIST: function (filelist) {
-    var list = "<ul>";
+  CONTENT: function (filelist, date) {
+    var contents = `<tr><td>asdf</td></tr>`;
     for (var i = 0; i < filelist.length; i++) {
-      list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+      var first_line = fs
+        .readFileSync(`"${filelist[i]}.txt"`)
+        .toString()
+        .split("\n");
+      // contents += `<tr>
+      // <td>${i}</td>
+      // <td>${title}</td>
+      // <td>${first_line}</td>
+      // <td>${date}</td>
+      // </tr>`;
     }
-    list += "</ul>";
-    return list;
+    return contents;
   },
 };
 module.exports = template;
